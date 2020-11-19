ddd62ce2a8b36a60a3a4dfc4e20a168a4021a911
Afia05 <72758634+Afia05@users.noreply.github.com>
2020-11-19 14:52:42 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-tempf = 70
-if tempf > 60:
-    light.set_pixel_color(5, light.rgb(250, 0, 0))
+tempf = 100
+while True:
+    print("temperature: " + input.temperature(TemperatureUnit.FAHRENHEIT))
+    if tempf > 60:
+        light.set_pixel_color(0, light.rgb(250, 0, 0))
+        light.set_pixel_color(1, light.rgb(250, 0, 0))
+        light.set_pixel_color(2, light.rgb(250, 0, 0))
+        light.set_pixel_color(3, light.rgb(250, 0, 0))
+        light.set_pixel_color(4, light.rgb(250, 0, 0))
+        light.set_pixel_color(5, light.rgb(250, 0, 0))
+        light.set_pixel_color(6, light.rgb(250, 0, 0))
+        light.set_pixel_color(7, light.rgb(250, 0, 0))
+        light.set_pixel_color(8, light.rgb(250, 0, 0))
+        light.set_pixel_color(9, light.rgb(250, 0, 0))
+    elif 70 < tempf > 40:
+        light.set_pixel_color(0, light.rgb(0, 250, 0))
+        light.set_pixel_color(1, light.rgb(0, 250, 0))
+        light.set_pixel_color(2, light.rgb(0, 250, 0))
+        light.set_pixel_color(3, light.rgb(0, 250, 0))
+        light.set_pixel_color(4, light.rgb(0, 250, 0))
+        light.set_pixel_color(5, light.rgb(0, 250, 0))
+        light.set_pixel_color(6, light.rgb(0, 250, 0))
+        light.set_pixel_color(7, light.rgb(0, 250, 0))
+        light.set_pixel_color(8, light.rgb(0, 250, 0))
+        light.set_pixel_color(9, light.rgb(0, 250, 0))
+    else:
+        light.set_pixel_color(0, light.rgb(0, 0, 250))
+        light.set_pixel_color(1, light.rgb(0, 0, 250))
+        light.set_pixel_color(2, light.rgb(0, 0, 250))
+        light.set_pixel_color(3, light.rgb(0, 0, 250))
+        light.set_pixel_color(4, light.rgb(0, 0, 250))
+        light.set_pixel_color(5, light.rgb(0, 0, 250))
+        light.set_pixel_color(6, light.rgb(0, 0, 250))
+        light.set_pixel_color(7, light.rgb(0, 0, 250))
+        light.set_pixel_color(8, light.rgb(0, 0, 250))
+        light.set_pixel_color(9, light.rgb(0, 0, 250))
--- main.ts
+++ main.ts
@@ -1,5 +1,39 @@
-let tempf = 70
-if (tempf > 60) {
-    light.setPixelColor(5, light.rgb(250, 0, 0))
+let tempf = 100
+while (true) {
+    console.log("temperature: " + input.temperature(TemperatureUnit.Fahrenheit))
+    if (tempf > 60) {
+        light.setPixelColor(0, light.rgb(250, 0, 0))
+        light.setPixelColor(1, light.rgb(250, 0, 0))
+        light.setPixelColor(2, light.rgb(250, 0, 0))
+        light.setPixelColor(3, light.rgb(250, 0, 0))
+        light.setPixelColor(4, light.rgb(250, 0, 0))
+        light.setPixelColor(5, light.rgb(250, 0, 0))
+        light.setPixelColor(6, light.rgb(250, 0, 0))
+        light.setPixelColor(7, light.rgb(250, 0, 0))
+        light.setPixelColor(8, light.rgb(250, 0, 0))
+        light.setPixelColor(9, light.rgb(250, 0, 0))
+    } else if (70 < tempf && tempf > 40) {
+        light.setPixelColor(0, light.rgb(0, 250, 0))
+        light.setPixelColor(1, light.rgb(0, 250, 0))
+        light.setPixelColor(2, light.rgb(0, 250, 0))
+        light.setPixelColor(3, light.rgb(0, 250, 0))
+        light.setPixelColor(4, light.rgb(0, 250, 0))
+        light.setPixelColor(5, light.rgb(0, 250, 0))
+        light.setPixelColor(6, light.rgb(0, 250, 0))
+        light.setPixelColor(7, light.rgb(0, 250, 0))
+        light.setPixelColor(8, light.rgb(0, 250, 0))
+        light.setPixelColor(9, light.rgb(0, 250, 0))
+    } else {
+        light.setPixelColor(0, light.rgb(0, 0, 250))
+        light.setPixelColor(1, light.rgb(0, 0, 250))
+        light.setPixelColor(2, light.rgb(0, 0, 250))
+        light.setPixelColor(3, light.rgb(0, 0, 250))
+        light.setPixelColor(4, light.rgb(0, 0, 250))
+        light.setPixelColor(5, light.rgb(0, 0, 250))
+        light.setPixelColor(6, light.rgb(0, 0, 250))
+        light.setPixelColor(7, light.rgb(0, 0, 250))
+        light.setPixelColor(8, light.rgb(0, 0, 250))
+        light.setPixelColor(9, light.rgb(0, 0, 250))
+    }
+    
 }
-
