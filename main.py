6f91bf0b0b6406764ef30f3255bd2f92abc9df7c
Afia05 <72758634+Afia05@users.noreply.github.com>
2020-11-19 12:53:01 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+tempf = 70
+if tempf > 60:
+    light.set_pixel_color(5, light.rgb(250, 0, 0))
---
+++ main.ts
@@ -0,0 +1,5 @@
+let tempf = 70
+if (tempf > 60) {
+    light.setPixelColor(5, light.rgb(250, 0, 0))
+}
+
